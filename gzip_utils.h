6a1f647571fa798f403e592c9739d64479605e00
ansond <ansond@localhost>
2017-02-28 16:52:08 +0000

    updated header

--- gzip_utils.h
+++ gzip_utils.h
@@ -5,7 +5,7 @@
 #include "zlib.h"
 
 // external declarations
-extern int gzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
-extern int gunzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
+extern "C" int gzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
+extern "C" int gunzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
 
 #endif // __GZIP_UTILS_H__
