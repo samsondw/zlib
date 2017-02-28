afcc5851b7cb28f304cde50a415693b2aa03067e
ansond <ansond@localhost>
2017-02-28 16:46:05 +0000

    updated header

--- gzip_utils.h
+++ gzip_utils.h
@@ -1,6 +1,9 @@
 #ifndef __GZIP_UTILS_H__
 #define __GZIP_UTILS_H__
 
+#include "mbed.h"
+#include "zlib.h"
+
 // external declarations
 extern int gzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
 extern int gunzip(unsigned char *dst, unsigned long *dst_length, unsigned char *src, unsigned long src_length);
